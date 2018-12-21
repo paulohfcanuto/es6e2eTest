e676aae2f0ffbd130310c488e44b3be9d762d6f3
Paulo Canuto <paulohfcanuto@gmail.com>
2018-12-21 17:23:22 -0200

    Update hooks

--- src/support/hooks.js
+++ src/support/hooks.js
@@ -2,14 +2,14 @@ const { BeforeAll, After } = require("cucumber");
 const conf = require(process.env.configLocation).config;
 
 
-BeforeAll({timeout: -1}, function() {
+BeforeAll({timeout: -1}, () => {
   return browser.get(conf.baseUrl);
 });
 
 After(function() {
-  /*const attach = this.attach;
+  const attach = this.attach;
   return browser.takeScreenshot().then(function(png) {
     const decodedImage = new Buffer(png, "base64");
     attach(decodedImage, "image/png");
-  });*/
+  });
 });
